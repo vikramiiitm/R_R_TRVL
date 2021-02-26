e846e0019edf1d4cff60412068a7de2a8f22bf1a
vikramiiitm <vikramiiitmc@gmail.com>
2021-02-26 12:43:00 +0530

    started the project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+const Navbar = () => {
+    return (
+    <>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link path="/" className></Link>
+            </div>
+        
+        </nav>
+    </>
+    );
+}
+
+export default Navbar;
